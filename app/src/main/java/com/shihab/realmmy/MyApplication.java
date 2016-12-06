e256ec5be50e3fc97f1dc1ff30f32b1044bb924e
shihabmi7 <shihab.mi7@gmail.com>
2016-12-06 18:28:29 +0600

    My Application

---
+++ app/src/main/java/com/shihab/realmmy/MyApplication.java
@@ -0,0 +1,36 @@
+package com.shihab.realmmy;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
+
+import java.util.regex.Pattern;
+
+
+/**
+ * Created by ASUS on 5/30/2016.
+ */
+    public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
+                        .build());
+
+        RealmInspectorModulesProvider.builder(this)
+                .withFolder(getCacheDir())
+
+                .withMetaTables()
+                .withDescendingOrder()
+                .withLimit(1000)
+                .databaseNamePattern(Pattern.compile(".+\\.realm"))
+                .build();
+    }
+
+}
